d07aaf9122c79baea88f9b833304990c9b030c20
Ridwan Ijadunola <dareedyone@gmail.com>
2021-09-02 09:57:51 +0100

    Heroku

--- src/services/otp.js
+++ src/services/otp.js
@@ -3,3 +3,4 @@ import config from "../helper/config";
 const { TOTP_KEY } = config;
 const token = totp(TOTP_KEY, { digits: 6 });
 const creteOneTitmeToken = () => token;
+export default creteOneTitmeToken;
