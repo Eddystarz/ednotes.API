9105933d8fd4a2ca967a5e5fd20c107849170b83
Ridwan Ijadunola <dareedyone@gmail.com>
2021-09-26 14:49:21 +0100

    res bug fixing

--- src/helper/rest_middleware.js
+++ src/helper/rest_middleware.js
@@ -4,20 +4,31 @@ const { JWT_SECRET_KEY } = config;
 
 const getTokenFrom = (request) => {
 	const authorization = request.get("authorization");
-	if (authorization && authorization.toLowerCase().startsWith("Bearer "))
+	// console.log("autho", authorization);
+	if (authorization && authorization.toLowerCase().startsWith("bearer "))
 		return authorization.substring(7);
 	return null;
 };
 
 export const isAuthenticated = async (req, res, next) => {
-	const token = getTokenFrom(req);
-	const decodedToken = jwt.verify(token, JWT_SECRET_KEY);
-	if (!token || !decodedToken.userId)
-		return res.status(401).json({
-			message: "Unauthorized user !",
-			value: false,
-		});
-	// console.log("the decoded token", decodedToken);
-	req.app.locals.authenticated = decodedToken;
-	next();
+	try {
+		const token = getTokenFrom(req);
+		// console.log("the token", token);
+		if (!token)
+			return res.status(401).json({
+				message: "Unauthorized user !",
+				value: false,
+			});
+		const decodedToken = jwt.verify(token, JWT_SECRET_KEY);
+		if (!token || !decodedToken.userId)
+			return res.status(401).json({
+				message: "Unauthorized user !",
+				value: false,
+			});
+		console.log("the decoded token", decodedToken);
+		req.app.locals.authenticated = decodedToken;
+		next();
+	} catch (error) {
+		console.log("res_middlware", error);
+	}
 };
--- src/services/notes.js
+++ src/services/notes.js
@@ -6,33 +6,45 @@ import Student from "../database/Models/student";
 export const getAttachment = async (req, res) => {
 	try {
 		// use req params later to check sub
-
-		const { Id } = req.app.locals.authenticated;
+		console.log("enter get");
+		const { userId } = req.app.locals.authenticated;
 		// const param = req.params;
 		const proxy_url = url.format({
 			protocol: req.protocol,
 			host: req.get("host"),
 			pathname: req.originalUrl,
 		});
-		const student = await Student.findOne({ user: Id });
+		console.log(proxy_url);
+		const student = await Student.findOne({ user: userId });
+		console.log("student", student);
 		const note = await LectureNote.findOne({
 			"noteAttachments.proxy_url": proxy_url,
-		}).populate("course");
+		});
+		// .populate("course");
+
+		console.log("note", note);
 
 		// change to sub check later by using level
 		const rightCourse =
 			student?.dept === note?.course.dept &&
 			student?.level === note?.course.level;
-		if (!student || !rightCourse) {
-			throw new Error("You are not authorized to access this resource !");
+		console.log("right", !rightCourse, !student, !student && !rightCourse);
+		if (!student || !note || !rightCourse) {
+			console.log("return");
+			return res.status(401).send({
+				message: "You are not authorized to access this resource !",
+				value: false,
+			});
 		}
 
 		const cloudAttachmentUrl = note.noteAttachments.find(
 			(attachment) => attachment.proxy_url === proxy_url
 		);
 		const data = await axios.get(cloudAttachmentUrl.url).data;
+		console.log("data", data);
 		res.send(data);
 	} catch (err) {
-		throw err;
+		console.log(err);
+		// throw err;
 	}
 };
